8f2d25e1f9b7ccbf5c93fc497cba03a43a907cab
OLUWABUKUNMI  OJO <88961905+Chrysz-Ojo@users.noreply.github.com>
2023-09-26 20:10:46 +0100

    Update SchoolApplication.java

--- school/src/main/java/com/ibk/school/SchoolApplication.java
+++ school/src/main/java/com/ibk/school/SchoolApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolApplication {
-
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
